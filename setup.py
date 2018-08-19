707dd6347b9d3b8b9f7463ac47a931f0413d03b1
Martin <martin@hotmail.com.br>
2018-08-19 03:22:43 -0300

    minor fixes

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ from setuptools import setup
 setup(
     name='ava_rememberme',
     packages=['ava_rememberme'],
+    version='0.1.0',
     author='Martin Mariano',
     author_email='contato@martinmariano.com',
     include_package_data=True,
     install_requires=[
-        'flask', 'flask-security', 'flask-sqlalchemy', 'beautifulsoup4', 'selenium', 'celery[redis]'
-
+        'flask', 'flask-security', 'flask-sqlalchemy', 'beautifulsoup4',
+        'selenium', 'celery[redis]', 'requests', 'lxml'
     ],
-    extras_require={
-        'psql': ['psycopg2']
-    }
-)
+    extras_require={'psql': ['psycopg2']})
