9b4b4f41b5571b7a8ff04653d21dbc70a1a129e8
Martin <martin@hotmail.com.br>
2018-08-27 01:20:10 -0300

    added localization package, fixed template pluralization

--- ava_rememberme/__init__.py
+++ ava_rememberme/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from secrets import token_hex
 from ava_rememberme.controller import create_celery
 from flask_migrate import Migrate
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = token_hex(32)
 app.config['SERVER_NAME'] = 'localhost:5000'
 
 celery = create_celery(app)
+babel = Babel(app, default_locale='br')
+
 
 from ava_rememberme.database import init_db, engine, Base
 from ava_rememberme import views
